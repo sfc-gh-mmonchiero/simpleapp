a21d5d5bd80c92d16ac8ff6e7bbbebe415705697
Matteo Monchiero <matteo@streamlit.io>
2020-09-21 15:04:18 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import sys
 
 st.title("Test App")
 
 value = st.slider("Pick a number", 0, 10, 3)
 
 st.write(value)
-print(value)
+sys.stdout.write(str(value) + "\n")
+sys.stdout.flush()
+#print(value)
