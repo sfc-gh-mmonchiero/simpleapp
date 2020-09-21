3db94f62a1e5bf7dda4510a7767252a6a45062a9
Matteo Monchiero <matteo@streamlit.io>
2020-09-21 14:54:00 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,8 @@
 import streamlit as st
 
-st.title("Test App new version!!!")
+st.title("Test App")
 
 value = st.slider("Pick a number", 0, 10, 3)
 
 st.write(value)
-
-input = st.text_input("Tell me something", "Cantami o Diva")
-
-st.write("Streamlit is fabulous")
-
-print("this is a log line with ansi code:  \e[101mLight red")
-print("Hello Matteo")
-print("From the user:", input)
+print(value)
