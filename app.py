e9ec3fc91025f30f0f2077d3c35616d66c1799f7
Matteo Monchiero <matteo@streamlit.io>
2020-10-13 15:08:02 -0700

    workers

--- app.py
+++ app.py
@@ -3,6 +3,27 @@ import sys
 
 st.title("Test App 11")
 
-counter = 1
-while True:
-  counter += 1
+from multiprocessing import Pool
+
+
+work_items = [
+    (1, 'A', True),
+    (2, 'X', False),
+]
+# Define a callable to do the work. It should take one work item.
+def worker(arg):
+    counter = 1
+    while True:
+      counter += 1
+
+# Create a ThreadPool (or a process Pool) of desired size.
+# What size? Experiment. Slowly increase until it stops helping.
+pool = Pool(2)
+
+# Do work and collect results.
+# Or use pool.imap() or pool.imap_unordered().
+work_results = pool.map(worker, work_items)
+
+# Wrap up.
+pool.close()
+pool.join()
