94048247b3fb52d90f30899e48dc6607adb158f5
Matteo Monchiero <matteo.monchiero@snowflake.com>
2023-03-08 14:36:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ import os
 st.title("Test App 57")
 
 value = st.slider("Pick a number", 0, 10, 3)
+
+option = st.selectbox('Make a choice', ['A', 'B'])
+
+st.write(option)
+
 st.write(os.getcwd())
 
 f = open("text", "a")
